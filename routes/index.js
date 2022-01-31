83c57d342990b7b4e59b0c8c97ac737ec5b69418
mr-kotresh-gb <kotreshgb4u@gmail.com>
2022-01-31 13:00:52 +0530

    Set routes and controllers

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ const homeController = require('../controllers/home_controller');
 console.log('Router loaded');
 
 router.get('/',homeController.home);
+router.use('/users', require('./users'));
+
+// For any further routes, access from here
+// router.use('/.....', require('./routerFile'));
+
+
+
+
 module.exports = router;
